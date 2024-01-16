f06f1a68f03938329704816e214901c85f70f606
rodrigosanchonet <80555128+rodrigosanchone@users.noreply.github.com>
2024-01-16 14:36:30 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ export const PORT = process.env.Port || 3000;
 export const DB_USER = process.env.DB_USER || ShadowRoot;
 export const DB_HOST = process.env.DB_HOST;
 export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_PORT = process.env.DB_PORT
 /* console.log(process.env.Port)
 console.log(process.env.DB_DATABASE) */
