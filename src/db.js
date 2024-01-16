0e60b5145ef7f44fc42ccf6ded39e08f99580bb1
rodrigosanchonet <80555128+rodrigosanchone@users.noreply.github.com>
2024-01-16 14:37:37 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,14 +3,16 @@ import {
     DB_HOST,
     DB_DATABASE,
     DB_PORT,
-    DB_USER
+    DB_USER,
+    DB_PASSWORD
 } from  './config.js';
 
 export const pool=createPool({
     host: DB_HOST,
     user:  DB_USER,
     port: DB_PORT,
-    database: DB_DATABASE
+    database: DB_DATABASE,
+    password: DB_PASSWORD
 })
 
 pool.query
